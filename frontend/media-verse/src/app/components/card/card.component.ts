c1a3a7fe51de1af859bebcd13f52ac6e172789ce
Zetneki <zoltaizeteny0@gmail.com>
2025-11-26 01:42:48 +0100

    changing card component style and home component popular movies display

--- frontend/media-verse/src/app/components/card/card.component.ts
+++ frontend/media-verse/src/app/components/card/card.component.ts
@@ -1,14 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie';
 import { TruncatePipe } from '../../pipes/truncate.pipe';
+import { KnobModule } from 'primeng/knob';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
-  imports: [TruncatePipe],
+  imports: [TruncatePipe, KnobModule, FormsModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   movie = input.required<Movie>();
+  value!: number;
+
+  ngOnInit(): void {
+    this.value = Math.round(this.movie().vote_average * 10) / 10;
+  }
+
   //itt majd ha tobb mindent akarok megjeleniteni lehet igy is <Movie | Book | VideoGame>
 }
